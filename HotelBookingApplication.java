93849d8ffab517e0137ccd28fc3a7e14ea56af7a
Madhav Bhardwaj <mb9770@srmist.edu.in>
2026-03-17 04:03:50 +0000

    Initial Commit

---
+++ HotelBookingApplication.java
@@ -0,0 +1,35 @@
+/**
+ * HotelBookingApplication
+ *
+ * This class represents the entry point of the Hotel Booking System.
+ * It demonstrates how a Java application starts execution using the
+ * main() method and prints basic startup information to the console.
+ *
+ * The program prints a welcome message along with the application
+ * name and version number before terminating.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class HotelBookingApplication {
+
+    /**
+     * The main method is the starting point of the Java application.
+     * The JVM invokes this method when the program is executed.
+     *
+     * @param args Command line arguments passed during program execution
+     */
+    public static void main(String[] args) {
+
+        // Print welcome message
+        System.out.println("Welcome to the Hotel Booking System");
+
+        // Print application name and version
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: v1.0");
+
+        // Inform that the application has started successfully
+        System.out.println("Application started successfully.");
+
+    }
+}
